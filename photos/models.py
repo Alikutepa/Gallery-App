ed2b1acdebb1f920b33808c9122130a67c50221d
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2021-11-27 21:55:29 +0300

    add location model

--- photos/models.py
+++ photos/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
     uniqueId = models.CharField(null=True, blank=True, max_length=100)
     slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
     date_created = models.DateTimeField(blank=True, null=True)
-    last_updated = models.DateTimeField(blank=True, null=True)
+    
 
     def __str__(self):
         return '{} {}'.format(self.title, self.uniqueId)
@@ -38,6 +38,28 @@ class Category(models.Model):
         super(Category, self).save(*args, **kwargs)
 
 
+class Location(models.Model):
+    name = models.CharField(max_length=60, default='')
+
+
+    def __str__(self):
+        return self.name
+    
+    @classmethod
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+    
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+  
 
 class Image(models.Model):
     description = models.TextField(null=True, blank=True)
@@ -45,12 +67,42 @@ class Image(models.Model):
     squareImage = ResizedImageField(size=[1000, 1000], crop=['middle', 'center'], default='default_square.jpg', upload_to='square')
     ##Related Fiels
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, null=True, on_delete=models.CASCADE)
 
     #Utility Variable
     uniqueId = models.CharField(null=True, blank=True, max_length=100)
     slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
     date_created = models.DateTimeField(blank=True, null=True)
-    last_updated = models.DateTimeField(blank=True, null=True)
+
+    @classmethod
+    def filter_by_location(cls, location):
+        image_location = Image.objects.filter(location__name=location).all()
+        return image_location
+
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+    
+    @classmethod
+    def search_by_category(cls, category):
+        images = cls.objects.filter(category__name__icontains=category)
+        return images
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    
 
     def __str__(self):
         return '{} {}'.format(self.category.title, self.uniqueId)
@@ -71,4 +123,4 @@ class Image(models.Model):
         self.slug = slugify('{} {}'.format(self.category.title, self.uniqueId))
         self.last_updated = timezone.localtime(timezone.now())
         super(Image, self).save(*args, **kwargs)
-  
+
