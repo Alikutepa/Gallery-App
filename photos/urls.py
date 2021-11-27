2a04e2bff0448540c698a365553247da900be3a1
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2021-11-27 23:22:37 +0300

    update change

--- photos/urls.py
+++ photos/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns =[
   path('',views.home, name='home'),
   path('category/<slug:slug>',views.categoryPage, name='image-category'),
-  path('category/<slug:slug1>/<slug:slug2>',views.imageDetailPage, name='image-detail')
+  path('category/<slug:slug1>/<slug:slug2>',views.imageDetailPage, name='image-detail'),
+  path('search/', views.search_image, name='search_image'),
 ]
