829290898bf13a0e5e0804dcf47b1cfc6993d1bd
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2021-11-27 19:46:41 +0300

    add url to image detail

--- photos/urls.py
+++ photos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns =[
   path('',views.home, name='home'),
-  path('category/<slug:slug>',views.categoryPage, name='image-category')
+  path('category/<slug:slug>',views.categoryPage, name='image-category'),
+  path('category/<slug:slug>',views.categoryPage, name='image-detail')
 ]
