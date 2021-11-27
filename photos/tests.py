ba6ce5af7a61a876bdacdb547fc991b3cbdf8635
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2021-11-28 00:23:51 +0300

    add tests

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Category,Location,Image
 
 # Create your tests here.
+
+# Set up method
+class CategoryTestClass(TestCase):
+
+  def setUp(self):
+    self.travel= Category(title = 'isiolo')
+
+  
+  def test_instance(self):
+        self.assertTrue(isinstance(self.travel,Category))
+
+# Set up method
+class LocationTestClass(TestCase):
+
+  def setUp(self):
+    self.nairobi = Location(title = 'town')
+  
+  def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi,Location))  
+
+
+# Set up method 
+# class ImageTestClass(TestCase):
