4ae1d57526aa1e3e0ed18594c4476ea62a811e7f
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2021-11-27 19:07:01 +0300

    add categorypage function

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+
 def home(request):
-  return render(request, 'main/index.html', {})
+    categories = Category.objects.all()
+    context = {}
+    context['categories'] = categories
+    return render(request, 'main/index.html', {})
+
+
+def categoryPage(request, slug):
+
+    category = Category.objects.get(slug=slug)
+    images = Image.objects.filter(category=category).order_by('-date_created')[:6]
+    for x in images:
+        x.shortDescription = x.description[:5]
+
+    context = {}
+    context['images'] = images
+    context['category'] = category
+
+    return render(request, 'main/category.html', context)
