94cfda78e212ed7dcfcea388aad090f0bdcf99f6
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2021-12-01 15:24:08 +0300

    make it better

--- gallery/settings.py
+++ gallery/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photos',
+    'photos.apps.PhotosConfig',
     'bootstrap5',
     'cloudinary',
 ]
